3d7faec2d111c3b9a225240c995596ebad7b3d88
hyejun <127915444+nojungsin@users.noreply.github.com>
2025-05-14 15:02:02 +0900

    Setting Change#npmstart안돼서.env추가

--- eng_learning/back/src/main/java/com/example/chat/Application.java
+++ eng_learning/back/src/main/java/com/example/chat/Application.java
@@ -18,7 +18,7 @@ public class Application {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3001")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
